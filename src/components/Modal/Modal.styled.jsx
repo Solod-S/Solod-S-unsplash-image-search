ce50bd44583c21a47d905faa85a6ef7b7d34902c
Sergey <104456968+Solod-S@users.noreply.github.com>
2022-10-17 15:34:14 +0300

    fix

--- src/components/Modal/Modal.styled.jsx
+++ src/components/Modal/Modal.styled.jsx
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 import { FiChevronLeft, FiChevronRight, FiSave } from 'react-icons/fi';
 
-import { BsDownload } from 'react-icons/bs';
 export const ModalOverlay = styled.div`
   position: fixed;
   top: 0;
