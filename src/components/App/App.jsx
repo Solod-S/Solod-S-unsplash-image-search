df77f7535adca40330ea33123dd70528dca91cef
Sergey <104456968+Solod-S@users.noreply.github.com>
2022-12-05 12:07:24 +0200

    test

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -35,7 +35,7 @@ export const App = () => {
           }
         />
         <Route
-          path="/favorite"
+          path="favorite"
           element={
             <FavoritePage
               images={images}
